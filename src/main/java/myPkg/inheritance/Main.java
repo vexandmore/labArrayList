5aa2b969739c95357034233c6b68bea8ee1bdec2
lauramarianella <lauramarianella@gmail.com>
2020-08-31 20:56:06 -0400

    Adding Main class

---
+++ src/main/java/myPkg/inheritance/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myPkg.inheritance;
+import java.util.Scanner;
+/**
+ *
+ * @author laura
+ */
+public class Main {
+
+    public static void main(String[] args) {  
+    Scanner input = new Scanner(System.in);
+    
+    //MyStack stack = new MyStack();
+    
+    /*for (int i = 0; i < 5; i++)
+      stack.push(input.nextLine());
+    
+    System.out.println(stack.getSize() + " strings are: ");
+    while (!stack.isEmpty())
+      System.out.println(stack.pop());*/
+  }
+}
