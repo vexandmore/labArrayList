dc094288daa6dea0935aa35de1f090378a475a57
Marc <Marc@Marc-PC>
2020-09-01 10:24:21 -0400

    start casting

--- src/main/java/myPkg/casting/Casting.java
+++ src/main/java/myPkg/casting/Casting.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package myPkg.casting;
-
+import java.util.*;
 /**
  *
  * @author Marc-Scattolin
@@ -12,5 +12,18 @@ package myPkg.casting;
 public class Casting {
 
     public static void main(String[] args) {  
+		ArrayList<Object> list = new ArrayList<>();
+		list.add(new Circle(2.5));
+		list.add(new Date());
+		list.add("Hello");
+		list.add(new Rectangle(2.0,3.2));
+		
+		for (Object obj: list) {
+			if (obj instanceof Circle) {
+				Circle circle = (Circle) obj;
+				System.out.println(circle.getRadius());
+			}
+			
+		}
     }
 }
