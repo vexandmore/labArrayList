a9bbd09af8ef610bdfb60147167ffa9247dc47aa
Marc <Marc@Marc-PC>
2020-09-01 10:56:41 -0400

    add comments and change return of push

--- src/main/java/myPkg/inheritance/MyStack.java
+++ src/main/java/myPkg/inheritance/MyStack.java
@@ -11,26 +11,31 @@ import java.util.ArrayList;
  *
  * @author Marc-Scattolin
  */
-public class MyStack<E> extends ArrayList<E> {
-
+public class MyStack extends ArrayList {
+	//returns true if the stack is empty
 	public boolean isEmpty() {
 		return size() == 0;
 	}
-
+	
+	//get the number of elements in the stack
 	public int getSize() {
 		return size();
 	}
-
-	public E peek() {
+	
+	//Get the object at the top of the stack without removing it
+	public Object peek() {
 		return get(size() - 1);
 	}
-
-	public E pop() {
+	
+	//remove and returns the element at the top of the stack
+	public Object pop() {
 		return remove(size() - 1);
 	}
-
-	public void push(E element) {
+	
+	//add an element to the stack and return it
+	public Object push(Object element) {
 		add(element);
+		return element;
 	}
 }
 
